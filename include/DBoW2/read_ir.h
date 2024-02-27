b9dba8f1dd29e7074aa688025666feb9cf95f7d6
Colin Keil <keil.c@northeastern.edu>
2024-02-27 17:31:28 -0500

    small updates

--- include/DBoW2/read_ir.h
+++ include/DBoW2/read_ir.h
@@ -6,6 +6,7 @@
 #include <opencv2/core/types.hpp>
 
 void readDescNPY(std::string const &data_fname, cv::Mat &out);
+void readDescNPY(std::string const &data_fname, std::vector<cv::Mat> &out);
 void readDescNPY(std::string const &data_fname, std::vector<std::vector<float>> &out);
 // void readKpNPY(std::string const &data_fname, std::vector<cv::KeyPoint> &keypoints);
 #endif
